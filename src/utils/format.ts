3d0b403ce20562af3ea3bc7c8bf170abefeeb1bd
sumitsgr <sumitsagar0010@gmail.com>
2026-05-25 11:39:43 +0530

    clean old root frontend files

--- src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-import { default as dayjs } from 'dayjs';
-
-export const formatDate = (date?: number | string | Date | null) => {
-  if (!date) return '-';
-
-  return dayjs(date).format('MMMM D, YYYY h:mm A');
-};
