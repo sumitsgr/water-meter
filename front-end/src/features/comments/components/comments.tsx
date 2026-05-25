4c60624503dd1a89ad54e2f2f1e2e91fbd16ba27
sumitsgr <sumitsagar0010@gmail.com>
2026-05-25 14:58:32 +0530

    new ip

--- front-end/src/features/comments/components/comments.tsx
+++ front-end/src/features/comments/components/comments.tsx
@@ -9,7 +9,7 @@ export const Comments = ({ discussionId }: CommentsProps) => {
   return (
     <div>
       <div className="mb-4 flex items-center justify-between">
-        <h3 className="text-xl font-bold">Comments:</h3>
+        <h3 className="text-xl font-bold">Comments :-</h3>
         <CreateComment discussionId={discussionId} />
       </div>
       <CommentsList discussionId={discussionId} />
