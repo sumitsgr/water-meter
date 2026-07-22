be5a4d76e0c262fa575950fd7d3b6d353fc8041d
sumitsgr <sumitsagar0010@gmail.com>
2026-07-22 17:01:02 +0530

    ad

--- front-end/src/features/testing/components/todo-list.tsx
+++ front-end/src/features/testing/components/todo-list.tsx
@@ -1,21 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const TodoList = () => {
-  const [todo, settodo] = useState([]);
-  console.log(todo);
-
-  const handleClick = () => {};
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="add todos..."
-        onChange={(e) => settodo(e.target.value)}
-      />
-      <button onClick={handleClick}>Add Todo</button>
-    </div>
-  );
+export const Todolist = () => {
+  return <div>Todolist</div>;
 };
-
-export default TodoList;
